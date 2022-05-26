3ce453fb0f59b0b060fdc36313927cb125af91ce
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2022-05-26 10:10:07 -0500

    Correcciones 8
    Se aplican ajustes.

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -11,7 +11,7 @@
       .col-lg-10
         figure.mb-5
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/WjILQvAUt_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     .bloque-texto-a.color-secundario.p-4.p-md-4.mb-5(data-aos="flip-right")
       .row.m-0.align-items-center.justify-content-between
         .col-lg-3.mb-4.mb-lg-0
